89bb32eece2dfaa7fbe5564c83db63f79648d058
Bob <103444193@student.swin.edu.au>
2023-06-08 14:24:18 +0700

    Add in the ability to verify string password with the existing salt and hash

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,8 @@ export function genPassword(password : string) : GeneratedPassword {
         hash : genHash
     }
 }
+
+export function verifyPassword(password : string, hash : string, salt : string) : boolean {
+    let hashVerified = pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex')
+    return hash === hashVerified
+}

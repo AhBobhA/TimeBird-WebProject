e0cd3c5d1a7b572d2dcd0caae1ae5d1a5768eff0
Bob <103444193@student.swin.edu.au>
2023-07-19 22:44:31 +0700

    Adjusted the schema of Enrollment: student_id -> user_id and user_id : string -> number

--- models/enrollment.ts
+++ models/enrollment.ts
@@ -4,7 +4,7 @@ export default class Enrollment {
     constructor(
         public unit_id : string,
         public semester_id : string,
-        public user_id : string,
+        public user_id : number,
         public enrolled_date : number,
         public result : number,
         public updated_at : number,

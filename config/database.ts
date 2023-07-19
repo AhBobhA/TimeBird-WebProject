72f355b12821e015b2d36d471fbf5aefbb4878fe
Bob <103444193@student.swin.edu.au>
2023-07-19 21:36:17 +0700

    Added a test route for student dashboard @/test/get_current_sem_stu

--- config/database.ts
+++ config/database.ts
@@ -5,7 +5,8 @@ export const collections : {
     users? : mongodb.Collection, 
     semester? : mongodb.Collection, 
     course? : mongodb.Collection, 
-    units? : mongodb.Collection } = {}
+    units? : mongodb.Collection
+    enrollments? : mongodb.Collection } = {}
 
 export async function connectToDb () {
     const db_uri = process.env.DB_CONN
@@ -14,10 +15,12 @@ export async function connectToDb () {
     const db_collection_sem = process.env.DB_COLLECTION_SEMESTER
     const db_collection_course = process.env.DB_COLLECTION_COURSE
     const db_collection_units = process.env.DB_COLLECTION_UNITS
+    const db_collection_enrollments = process.env.DB_COLLECTION_ENROLLMENTS
 
     if (db_uri != null && db_name != null 
         && db_collection_user != null && db_collection_sem != null 
-        && db_collection_course != null && db_collection_units != null) {
+        && db_collection_course != null && db_collection_units != null
+        && db_collection_enrollments != null) {
 
         const client : mongodb.MongoClient = new mongodb.MongoClient(db_uri)
         await client.connect()
@@ -27,14 +30,16 @@ export async function connectToDb () {
         const collection_sem : mongodb.Collection = db.collection(db_collection_sem)
         const collection_course : mongodb.Collection = db.collection(db_collection_course)
         const collection_units : mongodb.Collection = db.collection(db_collection_units)
+        const collection_enrollments : mongodb.Collection = db.collection(db_collection_enrollments)
 
         collections.users = collection_user
         collections.semester = collection_sem
         collections.course = collection_course
         collections.units = collection_units
+        collections.enrollments = collection_enrollments
 
         console.log(`Successfully connected to the database.\nDatabase name: ${db_name}
-                    \nCollections: ${db_collection_user}, ${db_collection_sem}, ${db_collection_course}, ${db_collection_units}`)
+                    \nCollections: ${db_collection_user}, ${db_collection_sem}, ${db_collection_course}, ${db_collection_units}, ${db_collection_enrollments}`)
     } else {
         console.log(`Missing environment variables.`)
     }
--- routes/test.ts
+++ routes/test.ts
@@ -2,7 +2,7 @@ import {Router, Request, Response}  from 'express'
 import { collections } from '../config/database'
 import Semester from '../models/semester'
 import Course from '../models/courses'
-import Unit from '../models/unit'
+import User from '../models/user'
 
 const router = Router()
 
@@ -10,7 +10,14 @@ router.get('/get_1', (req : Request, res : Response) => {
     res.send('1')
 })
 
-router.get('/get_current_sem', async (req : Request, res : Response) => {
+/*--------------------------------------------------------
+Description:        This end point return the current semester 
+                    name and all the associated courses along
+                    with their ID and descriptions.
+Request content:    None
+Response content:   Semester name, all courses in that semester.
+--------------------------------------------------------*/
+router.get('/get_current_sem_reg', async (req : Request, res : Response) => {
     const current_date = Date.now()
     const current_sem = await collections.semester?.findOne({
         start_date: {$lte: current_date}, 
@@ -21,19 +28,52 @@ router.get('/get_current_sem', async (req : Request, res : Response) => {
     const current_course = await collections.course?.findOne({semester_id: current_sem.semester_id}) as Course
     console.log(current_course)
 
-    let current_units : Unit[] = []
     collections.units?.find(
         {unit_id: {$in: current_course.unit_id} }
     ).toArray().then(data => {
-        current_units = data as Unit[]
-    }).finally(() => {
-        const data = {
+        const res_data = {
             sem_name: current_sem.semester_id,
-            courses: current_units
+            courses: data
         }
-        res.status(200).send({status: "success", data})
+        res.status(200).send({status: "success", res_data})
     })
 })
+
+/*--------------------------------------------------------
+Description:        This end point return the current semester 
+                    name, the current student's name, and all 
+                    the courses that the student registered for 
+                    in that semester.
+Request content:    None
+Response content:   Student's first and last name. Current semester
+                    name. All courses' information.
+--------------------------------------------------------*/
+router.get('/get_current_sem_stu', async (req : Request, res : Response) => {
+    const current_date = Date.now()
+    const current_sem = await collections.semester?.findOne({
+        start_date: {$lte: current_date}, 
+        end_date: {$gte: current_date}
+    }) as Semester
+    console.log(current_sem)
+
+    const current_student = await collections.users?.findOne({user_id: 32}) as User
+    console.log(current_student)
+
+    collections.enrollments?.find(
+        {semester_id: current_sem.semester_id, student_id: "32"}
+    ).toArray().then(data => {
+        const res_data = {
+            student_fn: current_student.firstname,
+            student_ln: current_student.lastname,
+            courses: data,
+            start_date: current_sem.start_date,
+            end_date: current_sem.end_date
+        }
+        res.status(200).send({status: "success", res_data})
+    })
+}) 
+
+
 
 export default router
 
